8e8f209a601924748a97fce906a217a2e86e1895
Giuia <giuliadickel11@gmail.com>
2021-02-11 16:49:00 -0300

    Estudo Java Script

---
+++ jsstudy.js
@@ -0,0 +1,23 @@
+<script>
+document.getElementById("demo").innerHTML = "My First JavaScript";
+
+document.getElementById("demo").innerHTML = 5 + 6;
+
+document.write(5 + 6);
+
+var a, b, c;
+a = 5;
+b = 6;
+c = a + b;
+document.getElementById("demo1").innerHTML = c;
+
+var a, b, c;
+a = 5; b = 6; c = a + b;
+document.getElementById("demo1").innerHTML = c;
+
+document.getElementById("demo").innerHTML = 10.50;
+
+document.getElementById("demo").innerHTML = 'John Doe';
+
+
+</script>
