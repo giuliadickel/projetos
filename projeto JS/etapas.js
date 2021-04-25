197e6f61aaad1d6e0c958ea8dfe761b9f0c74d85
Giuia <giuliadickel11@gmail.com>
2021-04-25 15:37:13 -0300

    estudo d JS

---
+++ projeto JS/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'Vereador',
+        numero: '5',
+        candidatos: [
+            {
+                numero: 38111,
+                name: 'Fulano',
+                partido: 'ABC',
+                fotos:[
+                    { URL:'444.jpg', legenda: 'vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        numero: 77222 ,
+        name: 'Ciclano',
+        Partido: 'CDF',
+        fotos:[
+            {URL:'333.jpg', legenda: 'vereador'}
+        ]
+    },
+
+    {
+        titulo: 'Prefeito',
+        numero:'2',
+        candidatos: [
+            {
+                numero:88,
+                name: 'Beltrano',
+                partido: 'HTG',
+                Vice: 'Geralda',
+                fotos:[
+                    {URL: '111.jpg', legenda: 'Prefeito'},
+                    {URL: '222.jpg', legenda: 'Vice-prefeito', small:true}
+                ]
+            },
+
+            {
+                numero: 84,
+                name: 'Ciclaninho',
+                partido:'POS',
+                Vice: 'Mayor',
+                Fotos:[
+                    {URL:'download(1).jpg', legenda: 'Prefeito'},
+                    {URL:'download(2).jpg', legenda: 'Vice-prefeito',small: true}
+                ]
+            }
+        ]
+    }
+]
